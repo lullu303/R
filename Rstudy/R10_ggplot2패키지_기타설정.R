c46a2bf40beecea74c66ec790549294691e7c3c9
jihyepark <109124273+lullu303@users.noreply.github.com>
2023-03-29 11:53:50 +0900

    20230329_12:00

--- Rstudy/R10_ggplot2패키지_기타설정.R
+++ Rstudy/R10_ggplot2패키지_기타설정.R
@@ -54,11 +54,45 @@ ggplot(score,aes(이름,점수, fill=과목)) +
   geom_text(aes(label=점수), # stat='identity'가 디폴트
             position=position_stack(vjust=0.5))
 
+#########################################################
+g <- ggplot(mtcars, aes(hp,mpg))
+g + geom_point(shape=15, size=3, color="blue")
+
+# g + geom_point(shape=factor(gear),
+#                color=factor(gear)
+# Error in factor(gear) : object 'gear' not found
+# gear 컬럼이 그래프 배경에 매핑되지 않음
+
+# geom_point(aes()) 으로 필요 컬럼 매핑
+g + geom_point(aes(shape=factor(gear),
+               color=factor(gear)))
+
+# 그래프는 변수값에 의해 서로 다른 모양과 색상으로
+# 그려짐 ; 변수명을 인식했음
+# 보여주는 결과만 다르게 나옴
+# 변수의 값에 따라 내부 그룹이 생성된 건 아님
+# 범례 생성이 안됨
+g + geom_point(shape=factor(mtcars$gear),
+               color=factor(mtcars$gear))
+
+
+team <- c('ko','ko','ko','ch','ch','ch')
+grade <- c(1,2,3,1,2,3)
+points <- c(30,51,60,20,47,62)
+df_team <- data.frame(team,grade,points)
+View(df_team) 
+
+g <- ggplot(df_team, aes(grade, points))
+
+g + geom_line(aes(color=team, linetype=team)) +
+  geom_point(aes(shape=team))
 
 
 
-
-
+# 모든 그래프에 동일한 모양, 동일한 색상을 적용
+# 꾸미기 파라미터 적용 없이 그룹만 분류
+g + geom_line(aes(group=team), color='red') +
+  geom_point(aes(fill=team))
 
 
 
